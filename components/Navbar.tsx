fbc5ac959f7b2b5888973fd6a683d00c9345c74a
Ayush Kannaujiya <ayushkannaujiya@gmail.com>
2024-08-30 12:56:07 +0530

    removed stroke

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 
         <div className="hidden md:flex pt-10 h-full font-mono font-normal text-gray-300">
             <p className="px-5 flex">About us
-                <svg  stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" className="rotate-0 h-6 w-6 pt-2 pl-1 pb-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                <svg   fill="currentColor" viewBox="0 0 448 512" className="rotate-0 h-6 w-6 pt-2 pl-1 pb-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                 <path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 
                 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg>
             </p>
