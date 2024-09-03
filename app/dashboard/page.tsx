0fec4947b7b5b5f8489868f67777669f356d8eeb
Ayush Kannaujiya <ayushkannaujiya@gmail.com>
2024-09-03 14:10:34 +0530

    he

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ const dashboard = () => {
     return (
         <div>
             <div className="text-black flex justify-center">You are logged in</div>
-            <div className="text-black flex justify-center">This is the dashboard page and will be developed soon</div>
+            <div className="text-black flex justify-center">This is the dashboard page and will be developed soon.</div>
         </div>
         
     )
